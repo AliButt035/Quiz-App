5554ed37b52876f47b5b479002f20c822ad31236
Ali Iqbal <bcsf18m035@pucit.edu.pk>
2021-11-11 00:18:22 -0800

    Initial Commit

---
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button=findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openQuizActivity();
+            }
+        });
+    }
+    public void openQuizActivity()
+    {
+        Intent intent = new Intent(this,QuizActivity.class);
+        startActivity(intent);
+    }
+}
